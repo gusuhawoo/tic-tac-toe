9bf0c4382ead442fcd84901cafbe2e28ab95ac9e
suha <gusuhawoo@gmail.com>
2023-07-16 20:55:25 +0900

    Add 'O,X' Rule

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,11 +4,25 @@ import './Board.css';
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setSIsNext] = useState(true);
+  const status = `Next player: ${xIsNext ? 'x' : 'O'}`;
+
+  // const [number, setNumber] = useState(0);
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
+    newSquares[i] = xIsNext ? 'x' : 'O';
     setSquares(newSquares);
+    // setSIsNext(!xIsNext); // 첫번째방법 !true => false
+    setSIsNext((prev) => !prev); // 두번째방법 previous State => !previous State
+
+    // setSIsNext(!xIsNext);와 setSIsNext((prev) => !prev);의 사용시 차이점
+    // 중복할 경우 두번째 방법은 전부 처리가 되지만 첫번째 방법은 하나만 처리가됨
+    // setNumber(number + 1); // 첫번째방법
+    // setNumber(number + 1); // 첫번째방법
+    // setNumber((prev) => prev + 1); // 두번째방법
+    // setNumber((prev) => prev + 1); // 두번째방법
+    // console.log('number', number);
   };
 
   const renderSquare = (i) => {
@@ -16,10 +30,10 @@ const Board = () => {
     // 부모컴포넌트 : props를 내려줌
   };
 
-  const status = 'Next player: X';
-
   return (
     <div>
+      {/* {console.log('number2', number)} */}
+      {/* 첫번째경우:중복임에도 1씩증가, 두번째경우:중복이므로 2씩증가 */}
       <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}

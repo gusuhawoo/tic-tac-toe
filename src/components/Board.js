124e8b1384cfd6cdefdfb10f3cd8ddf00d29e1c5
suha <gusuhawoo@gmail.com>
2023-07-16 21:40:29 +0900

    Decide on the winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,12 +5,50 @@ import './Board.css';
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setSIsNext] = useState(true);
-  const status = `Next player: ${xIsNext ? 'x' : 'O'}`;
+
+  const calculateWinner = (squares) => {
+    // 모든 승리할 수 있는 경우의 수 나열
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next player: ${xIsNext ? 'x' : 'O'}`;
+  }
 
   // const [number, setNumber] = useState(0);
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
+    console.log('newSquares', newSquares);
+    console.log('newSquares[i]', newSquares[i]);
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
     newSquares[i] = xIsNext ? 'x' : 'O';
     setSquares(newSquares);
     // setSIsNext(!xIsNext); // 첫번째방법 !true => false

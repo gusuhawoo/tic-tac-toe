0970c8568a7abdb8d29e60102e3628a0f96e90be
suha <gusuhawoo@gmail.com>
2023-11-05 14:27:16 +0900

    Modify the content of the code.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board';
+import { useState } from 'react';
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    // 모든 승리할 수 있는 경우의 수 나열
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = `Next player: ${xIsNext ? 'x' : 'O'}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'x' : 'O';
+    setHistory([...history, { squares: newSquares }]);
+    setXIsNext((current) => !current);
+  };
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <div className="status">{status}</div>
         <ol>{/* todo */}</ol>
       </div>
     </div>
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,77 +2,13 @@ import React, { useState } from 'react';
 import Square from './Square';
 import './Board.css';
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setSIsNext] = useState(true);
-
-  const calculateWinner = (squares) => {
-    // 모든 승리할 수 있는 경우의 수 나열
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let index = 0; index < lines.length; index++) {
-      const [a, b, c] = lines[index];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
-  const winner = calculateWinner(squares);
-
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = `Next player: ${xIsNext ? 'x' : 'O'}`;
-  }
-
-  // const [number, setNumber] = useState(0);
-
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-    console.log('newSquares', newSquares);
-    console.log('newSquares[i]', newSquares[i]);
-    if (calculateWinner(newSquares) || newSquares[i]) {
-      return;
-    }
-
-    newSquares[i] = xIsNext ? 'x' : 'O';
-    setSquares(newSquares);
-    // setSIsNext(!xIsNext); // 첫번째방법 !true => false
-    setSIsNext((prev) => !prev); // 두번째방법 previous State => !previous State
-
-    // setSIsNext(!xIsNext);와 setSIsNext((prev) => !prev);의 사용시 차이점
-    // 중복할 경우 두번째 방법은 전부 처리가 되지만 첫번째 방법은 하나만 처리가됨
-    // setNumber(number + 1); // 첫번째방법
-    // setNumber(number + 1); // 첫번째방법
-    // setNumber((prev) => prev + 1); // 두번째방법
-    // setNumber((prev) => prev + 1); // 두번째방법
-    // console.log('number', number);
-  };
-
+const Board = ({ squares, onClick }) => {
   const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
-    // 부모컴포넌트 : props를 내려줌
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
 
   return (
     <div>
-      {/* {console.log('number2', number)} */}
-      {/* 첫번째경우:중복임에도 1씩증가, 두번째경우:중복이므로 2씩증가 */}
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
